253d019218e5a18fe73fa1e9f7049ee2b2bf71e1
Tonya Napoli <tonya@blackcaturbanfarm.com>
2024-09-21 00:17:41 -0400

    moved styling to App.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 //adding landing page
 import React from 'react';
+import '../App.css';
 
 const Home = () => {
     return (
-        <div style= {{ textAlign: 'center', padding: '20px'}}>
+        <div className="home">
             <h1>Welcome to Puppy Pros</h1>
             <p>Your one-stop solution for managing dog training sessions and clients information.</p>
             <button onClick={() =>alert('Coming Soon!')}>Get Started</button>
