59d4ec4591ecfc1289f8ba2be8d1fcc55a817cf6
Tonya Napoli <tonya@blackcaturbanfarm.com>
2024-09-28 21:18:25 -0400

    created login as logic

---
+++ src/pages/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {  useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import '../App.css'; 
+
+const Login = () => {
+    const { login } = useAuth(); //Use the login function from the AuthContext
+    const navigate = useNavigate();
+
+    const handleLogin = (role) => {
+        login(role); //Set user role as "trainer" or "client"
+        navigate('/'); //Navigate to the home page after logging in
+    };
+
+    return (
+        <div className="centered-container">
+            <h2>Login as:</h2>
+            <button className="btn" onClick={() => handleLogin('trainer')}>
+                Login as Trainer
+                </button>
+                <button className="btn" onClick={() => handleLogin('client')}>
+                Login as Client
+                </button>
+        </div>
+    );
+    };
+
+    export default Login;
+    
