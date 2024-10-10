988eff538f77fb3cfe45260c4a82a2e12548a48a
Tonya Napoli <tonya@blackcaturbanfarm.com>
2024-10-09 21:02:31 -0400

    mockUsers.js

---
+++ src/mocks/mockUsers.js
@@ -0,0 +1,20 @@
+const users = [
+    {
+      username: 'trainer1',
+      password: 'password123',
+      role: 'trainer',
+    },
+    {
+      username: 'client1',
+      password: 'password123',
+      role: 'client',
+    },
+    {
+      username: 'admin1',
+      password: 'password123',
+      role: 'admin',
+    },
+  ];
+  
+  export default users;
+  
