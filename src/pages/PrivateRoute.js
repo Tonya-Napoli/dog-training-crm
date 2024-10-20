2ca574cb309ef41028a31cf944233b84fa2ac3ce
Tonya Napoli <tonya@blackcaturbanfarm.com>
2024-10-20 19:47:56 -0400

    set up PrivateRoute

---
+++ src/pages/PrivateRoute.js
@@ -0,0 +1,14 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+const PrivateRoute = ({ children, role }) => {
+  const { user } = useAuth();
+
+  if (!user || user.role !== role) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
