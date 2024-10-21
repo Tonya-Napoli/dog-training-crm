84bbb1421c11b07730eeab03f8b52391ab7de6fc
Tonya Napoli <tonya@blackcaturbanfarm.com>
2024-10-21 16:13:41 -0400

    footer

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+
+
+const Footer = () => {
+    return (
+      <footer>...</footer>
+    );
+  };
+  export default Footer; // Make sure this is a default export
+  
