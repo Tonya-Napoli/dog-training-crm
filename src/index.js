252fd938b2dd7747c84a25085abe31b55d018743
Tonya Napoli <Tonya.goodell@outlook.com>
2025-01-11 10:01:33 -0500

    eliminated service-worker

--- src/index.js
+++ src/index.js
@@ -19,15 +19,3 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered: ', registration);
-      })
-      .catch(registrationError => {
-        console.log('Service Worker registration failed: ', registrationError);
-      });
-  });
-}
