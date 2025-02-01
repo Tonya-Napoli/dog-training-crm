c0b5eb0039146ebbe926f179ab297fd7181c89f6
Tonya Napoli <tonya@blackcaturbanfarm.com>
2025-01-31 23:01:57 -0500

    installed nodemon

--- src/pages/server.js
+++ src/pages/server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000; // Use the PORT from .env or default to 3
 
 //use cors middleware to allow cross-origin requests
 app.use(cors());
+app.options('*', cors());
+
 
 // Middleware
 app.use(bodyParser.json());
