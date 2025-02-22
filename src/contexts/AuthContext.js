4ba90e1b227c9cd10f7434518e9223f3b986f67e
Tonya Napoli <tonya@blackcaturbanfarm.com>
2025-02-21 21:47:21 -0500

    updated file name

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext } from 'react';
-import usersMock from '../mocks/usersMock';
+import usersMock from '../mocks/UsersMock';
 
 // Create a context for authentication and role management
 const AuthContext = createContext();
