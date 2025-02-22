8e48f92d9f118129df994b88adddbf06871b6008
Tonya Napoli <tonya@blackcaturbanfarm.com>
2025-02-21 22:54:12 -0500

    updated package.json and server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import api from './src/api';
+import app from './src/api';
 import { config } from 'dotenv';
 
 config();
 const port = process.env.PORT || 4000;
 
-api.listen(port, () => {
+app.listen(port, () => {
     console.log(`*************************\n \tPORT: ${port} \n*************************`);
 });
+
 
 
