a78d1dced24d96bb451fb693633bb2f5a4d49f02
Tonya Napoli <tonya@blackcaturbanfarm.com>
2025-03-01 23:59:11 -0500

    fixing server/client issues

--- src/pages/server.js
+++ src/pages/server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const sgMail = require('@sendgrid/mail');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Use CORS middleware to allow cross-origin requests
 app.use(cors());
