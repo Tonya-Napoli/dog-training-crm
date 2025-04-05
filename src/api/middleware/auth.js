909e28abdabff630fb263ded4eab1e34f2be816f
Tonya Napoli <tonya@blackcaturbanfarm.com>
2025-04-04 22:46:06 -0400

    added middleware and authroutes

---
+++ src/api/middleware/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+export default function auth(req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    // Set user data from token to request object
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token is not valid' });
+  }
+}
