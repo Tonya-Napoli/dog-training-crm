ef6892f335b3250ad7bfb76ce5e9e5f83a04916b
Tonya Napoli <tonya@blackcaturbanfarm.com>
2025-04-26 20:12:23 -0400

    created page component to render form ClientRegistraitonPage

---
+++ src/pages/ClientRegistrationPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/ClientRegistrationPage.jsx
+import React from 'react';
+import ClientForm from '../components/forms/ClientForm';
+
+const ClientRegistrationPage = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <ClientForm />
+    </div>
+  );
+};
+
+export default ClientRegistrationPage;
