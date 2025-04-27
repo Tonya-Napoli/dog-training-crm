819051a79dca358b487220834559f69d78b8cb9c
Tonya Napoli <tonya@blackcaturbanfarm.com>
2025-04-26 20:18:41 -0400

    added import and render page for TrainerRegistrationPage

---
+++ src/pages/TrainerRegrationPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TrainerForm from '../components/forms/TrainerForm';
+
+const TrainerRegistrationPage = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <TrainerForm />
+    </div>
+  );
+};
+
+export default TrainerRegistrationPage;
